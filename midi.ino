7f67e9d6b3a106c3f4776f5d198e5270bf2e2afa
cxdzb <1005968086@qq.com>
2019-05-22 16:43:44 +0800

    last

--- midi.ino
+++ midi.ino
@@ -1,86 +1,76 @@
-byte buffer[3];
+﻿byte buffer[3];
 int pwms[5]={255};
 int c[5]={3,5,6,9,10};
-int TIME;
+unsigned long TIME;
 
 void setup() {
   Serial.begin(9600);
   TIME=millis();
 }
 void loop() {
-  if(Serial.available()>=3)
-  {
-    int cmd_channel=Serial.read();
-    int first=Serial.read();
-    int second=Serial.read();
-    if(cmd_channel>>4==0xE){  //0xE
-      if((byte(cmd_channel&0xF)<8)&&(byte(first&0x7F)==0x11)&&(byte(second&0x7F)==0x11)){
-        int ad=analogRead(cmd_channel&0xF+14);
-        Serial.write((byte)cmd_channel);
-        Serial.write((byte)(ad & 0x7F));
-        Serial.write((byte)((ad >> 7) & 0x7F));
-      }
-    }
-    else if(cmd_channel>>4==0xD){ //0xD
+  if(millis()-TIME>=100){
+    TIME=millis();
+    if(Serial.available()>=3){
+      int cmd_channel=Serial.read();
+      int first=Serial.read();
+      int second=Serial.read();
       int Channel=cmd_channel&0xF;
-      for(int i=0;i<5;i++){
-        if(c[i]==Channel){
-          if(byte((second>>5)&0x1)){  //返回pwm值
-            Serial.write((byte)cmd_channel);
-            Serial.write((byte)(pwms[i] & 0x7F));
-            Serial.write((byte)(((pwms[i] >> 7) & 0x1)|second));
-          }
-          else{ //设定pwm值
-            pwms[i]=first|((second&0x1)<<7);
-            analogWrite(Channel,pwms[i]);
+      if(cmd_channel>>4==0xE){  //0xE
+        if((byte(cmd_channel&0xF)<8)&&(byte(first&0x7F)==0x11)&&(byte(second&0x7F)==0x11)){
+          int ad=analogRead(cmd_channel&0xF+14);
+          Serial.write((byte)cmd_channel);
+          Serial.write((byte)(ad & 0x7F));
+          Serial.write((byte)((ad >> 7) & 0x7F));
+        }
+      }
+      else if(cmd_channel>>4==0xD){ //0xD
+        for(int i=0;i<5;i++){
+          if(c[i]==Channel){
+            if(byte((second>>5)&0x1)){  //返回pwm值
+              Serial.write((byte)cmd_channel);
+              Serial.write((byte)(pwms[i] & 0x7F));
+              Serial.write((byte)(((pwms[i] >> 7) & 0x1)|second));
+            }
+            else{ //设定pwm值
+              pwms[i]=first|((second&0x1)<<7);
+              analogWrite(Channel,pwms[i]);
+            }
           }
         }
       }
-    }
-    else if(cmd_channel>>4==0xF){ //0xF
-      if((byte)cmd_channel==0xFF&&(byte)first==0x55&&(byte)second==0x55){ //时间
-        int current=millis()-TIME;
-        Serial.write((byte)cmd_channel);
-        Serial.write((byte)(current&0x7F));
-        Serial.write((byte)((current>>7)&0x7F));
+      else if(cmd_channel>>4==0xF){ //0xF
+        if((byte)cmd_channel==0xFF&&(byte)first==0x55&&(byte)second==0x55){ //时间
+          int current=millis();
+          Serial.write((byte)cmd_channel);
+          Serial.write((byte)(current&0x7F));
+          Serial.write((byte)((current>>7)&0x7F));
+        }
+        else if((byte)cmd_channel==0xF9&&(byte)first==0x55&&(byte)second==0x55){  //学号
+          int stu_num=8062;
+          Serial.write((byte)cmd_channel);
+          Serial.write((byte)(stu_num&0x7F));
+          Serial.write((byte)((stu_num>>7)&0x7F));
+        }
       }
-      else if((byte)cmd_channel==0xF9&&(byte)first==0x55&&(byte)second==0x55){  //学号
-        int stu_num=8062;
+      else if((byte)(cmd_channel>>4)==0x9&&(byte)second==0){
+        pinMode(Channel, OUTPUT);
+        if(first==0){
+          digitalWrite(Channel,LOW);
+        }
+        else if(first==1){
+          digitalWrite(Channel,HIGH);
+        }
+      }
+      else if((byte)(cmd_channel>>4)==0xC&&(byte)first==0x66&&(byte)second==0){  //pinMode(Channel, INPUT);
+        int state=digitalRead(Channel);
         Serial.write((byte)cmd_channel);
-        Serial.write((byte)(stu_num&0x7F));
-        Serial.write((byte)((stu_num>>7)&0x7F));
-      }      
-    }
-    else{ //无效
-      Serial.write((byte)0xF);
-      Serial.write((byte)0xA);
-      Serial.write((byte)0x7F);
+        Serial.write((byte)state);
+      }
+      else{ //无效
+        Serial.write((byte)0xF);
+        Serial.write((byte)0xA);
+        Serial.write((byte)0x7F);
+      }
     }
   }
-  else if(Serial.available()==2){
-    int cmd_channel=Serial.read();
-    int data=Serial.read();
-    int Channel=cmd_channel&0xF;
-    if((byte)(cmd_channel>>4)==0x9){
-      pinMode(Channel, OUTPUT);
-      if(data==0){
-        digitalWrite(Channel,LOW);
-      }
-      else if(data==1){
-        digitalWrite(Channel,HIGH);
-      }
-    }
-    else if((byte)(cmd_channel>>4)==0xC&&(byte)data==0x66){
-//      pinMode(Channel, INPUT);
-      int state=digitalRead(Channel);
-      Serial.write((byte)cmd_channel);
-      Serial.write((byte)state);
-    }
-    else{ //无效
-      Serial.write((byte)0xF);
-      Serial.write((byte)0xA);
-      Serial.write((byte)0x7F);
-    }
-  }
-  delay(100);
 }
